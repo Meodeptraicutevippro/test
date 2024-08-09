532f82dff4346db717010eef3b6faac2ffd90191
MeoMeo <166278097+Meodeptraicutevippro@users.noreply.github.com>
2024-08-09 16:54:47 +0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -760,62 +760,28 @@ local function wl(key)
         Size11,Size22 = 600,460
         if IsMobile then 
             Size11,Size22 = 500,290
-            local ClickButton = Instance.new("ScreenGui")
-            local MainFrame = Instance.new("Frame")
-            local ImageLabel = Instance.new("ImageLabel")
-            local TextButton = Instance.new("TextButton") 
-            local UICorner = Instance.new("UICorner") 
-            local UICorner_2 = Instance.new("UICorner")
-            if game.CoreGui:FindFirstChild("ClickButton") then 
-                game.CoreGui:FindFirstChild("ClickButton"):Destroy()
-            end
-            ClickButton.Name = "ClickButton"
-            ClickButton.Parent = game.CoreGui
-            ClickButton.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
-            
-            MainFrame.Name = "MainFrame"
-            MainFrame.Parent = ClickButton
-            MainFrame.Active = true
-            MainFrame.AnchorPoint = Vector2.new(0.5, 0.5)
-            MainFrame.BackgroundColor3 = Color3.new(1, 1, 1)
-            MainFrame.BorderColor3 = Color3.new(0, 0, 0)
-            MainFrame.BorderSizePixel = 0
-            MainFrame.Transparency = 1
-            MainFrame.Position = UDim2.new(0.187441245, 0, 0.476932675, 0)
-            MainFrame.Size = UDim2.new(0, 45, 0, 45)
-            
-            UICorner.CornerRadius = UDim.new(0, 100)
-            UICorner.Parent = MainFrame
-            
-            UICorner_2.CornerRadius = UDim.new(0, 100)
-            UICorner_2.Parent = ImageLabel
-            
-            ImageLabel.Parent = MainFrame
-            ImageLabel.AnchorPoint = Vector2.new(0.5, 0.5)
-            ImageLabel.BackgroundColor3 = Color3.new(0, 0, 0)
-            ImageLabel.BorderColor3 = Color3.new(0, 0, 0)
-            ImageLabel.BorderSizePixel = 0
-            ImageLabel.Position = UDim2.new(0.48888889, 0, 0.48888889, 0)
-            ImageLabel.Size = UDim2.new(0, 45, 0, 45)
-            ImageLabel.Image = "rbxassetid://15241946029"
-            
-            TextButton.Parent = MainFrame
-            TextButton.BackgroundColor3 = Color3.new(1, 1, 1)
-            TextButton.BackgroundTransparency = 1
-            TextButton.BorderColor3 = Color3.new(0, 0, 0)
-            TextButton.BorderSizePixel = 0
-            TextButton.Position = UDim2.new(3.3908421e-07, 0, 0, 0)
-            TextButton.Size = UDim2.new(0, 45, 0, 45)
-            TextButton.AutoButtonColor = false
-            TextButton.Font = Enum.Font.SourceSans
-            TextButton.Text = ""
-            TextButton.TextColor3 = Color3.new(255, 255, 255)
-            TextButton.TextSize = 15
-            TextButton.MouseButton1Click:Connect(function()
-                game:GetService("VirtualInputManager"):SendKeyEvent(true,"LeftControl",false,game)
-                game:GetService("VirtualInputManager"):SendKeyEvent(false,"LeftControl",false,game)
-            end)
-        end 
+            local ScreenGui = Instance.new("ScreenGui")
+local ImageButton = Instance.new("ImageButton")
+local UICorner = Instance.new("UICorner")
+local UIStroke = Instance.new("UIStroke")
+
+ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
+
+ImageButton.Parent = ScreenGui
+ImageButton.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+ImageButton.BorderSizePixel = 0
+ImageButton.Position = UDim2.new(0.100739375, 0, 0.121457487, 0)
+ImageButton.Size = UDim2.new(0, 60, 0, 60)
+ImageButton.Image = "rbxassetid://16137232629"
+ImageButton.MouseButton1Click:Connect(function()
+    game:GetService("VirtualInputManager"):SendKeyEvent(true,Enum.KeyCode.End,false,game)
+end)
+
+UICorner.Parent = ImageButton
+
+UIStroke.Color = Color3.fromRGB(255, 255, 255)
+UIStroke.Parent = ImageButton
         local Window =
             Fluent:CreateWindow(
             {
@@ -2640,7 +2606,7 @@ local function wl(key)
         function getBlueGear()
             if game.workspace.Map:FindFirstChild("MysticIsland") then
                 for i, v in pairs(game.workspace.Map.MysticIsland:GetChildren()) do
-                    if v:IsA("MeshPart") and v.MeshId == "rbxassetid://10153114969" then --and not v.CanCollide then
+                    if v:IsA("MeshPart") and v.MeshId == "rbxassetid://16137232629" then --and not v.CanCollide then
                         return v
                     end
                 end
@@ -2769,7 +2735,7 @@ local function wl(key)
             end
             for i, v in pairs(game:GetService("Workspace").Map.MysticIsland:GetDescendants()) do
                 if v:IsA("MeshPart") then
-                    if v.MeshId == "rbxassetid://6745037796" then
+                    if v.MeshId == "rbxassetid://16137232629" then
                         return v
                     end
                 end
